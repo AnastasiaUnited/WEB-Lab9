0aaa887924e7a24253c407871643e461d84dda40
Yanbellq <yanbellq@gmail.com>
2025-11-14 23:09:16 +0200

    Checking prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import '@styles/App.css'
-import ButtonUsage from '@components/Button'
+import React from 'react';
+import '@styles/App.css';
+import ButtonUsage from '@components/Button';
 
 const API_URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -10,11 +10,13 @@ function App() {
     const title = 'Hello World!';
 
     return (
-        <div className='flex items-center justify-center w-full h-full tg'>
-            {title}<br />{API_URL} + {API_KEY} + {PORT}
+        <div className="flex items-center justify-center w-full h-full tg">
+            {title}
+            <br />
+            {API_URL} + {API_KEY} + {PORT}
             <ButtonUsage />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
