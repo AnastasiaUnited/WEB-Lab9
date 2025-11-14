27fee9de7314fe8d42d00eaf04cf39b460a6755a
Yanbellq <yanbellq@gmail.com>
2025-11-14 22:26:04 +0200

    Checking eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import './App.css'
 import ButtonUsage from './components/Button'
 
 function App() {
+    const title = 'Hello World!';
 
     return (
         <div className='flex items-center justify-center w-full h-full'>
+            {title}
             <ButtonUsage />
         </div>
     )
